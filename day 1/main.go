90ee5f513dd564dcf798d22170a0d849f01eed30
Taurenov <isataevmihail@gmail.com>
2025-05-02 20:17:50 +0300

    Add files via upload

---
+++ day 1/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("Всё работат и на других языках?") // Тут мы пишем текст.
+	fmt.Println("9 + 2 =", 9+2) // тут мы делаем подсчёт
+	fmt.Printf("Число: %d\n", 5)
+}
